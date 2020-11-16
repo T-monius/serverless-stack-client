ac9b4e28232649c6955519ece6e381baa09831ac
mo mac <trrl.mo@hotmail.com>
2020-11-16 15:18:15 -0700

    Create Logout button in App component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import { AppContext } from "./libs/contextLib";
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
 
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
@@ -20,12 +24,18 @@ function App() {
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {isAuthenticated ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <Nav.Link>Signup</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
