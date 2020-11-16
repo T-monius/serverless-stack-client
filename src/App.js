93cd35816511b28c465f4833f6dceffa197beb50
mo mac <trrl.mo@hotmail.com>
2020-11-16 15:28:20 -0700

    Add 'useEffect' hook for initial session authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  useEffect(() => {
+      onLoad();
+    }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+      setIsAuthenticating(false);
+    }
 
   function handleLogout() {
     userHasAuthenticated(false);
