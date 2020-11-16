d85366b8f5fca1d43f9eed701f925a0104e168fe
mo mac <trrl.mo@hotmail.com>
2020-11-16 15:42:09 -0700

    Add 'isLoading' state to the 'Login' component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -11,6 +11,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -19,12 +20,15 @@ export default function Login() {
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
       await Auth.signIn(email, password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
       alert(e.message);
+      setIsLoading(false);
     }
   }
 
