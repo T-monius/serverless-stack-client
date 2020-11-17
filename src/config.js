a1546d98babf507535a7d5bfb28119460c1088cb
mo mac <trrl.mo@hotmail.com>
2020-11-16 17:55:31 -0700

    Debug Amplify config for Cognito region; add a dash

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
     URL: "https://wwpejefpih.execute-api.us-east-1.amazonaws.com/prod",
   },
   cognito: {
-    REGION: "us-west2",
+    REGION: "us-west-2",
     USER_POOL_ID: "us-west-2_VpvThHFXS",
     APP_CLIENT_ID: "2192j2cb9tu5n93224e61d89i1",
     IDENTITY_POOL_ID: "us-west-2:0bde57ee-89c9-467a-98c7-bb09cebdb988",
