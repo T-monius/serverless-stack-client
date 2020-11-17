d579f8dafab6b6c23f13790f48570ce09172ec0b
mo mac <trrl.mo@hotmail.com>
2020-11-17 12:39:30 -0700

    Add a use effect to Home component to load notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,11 +3,35 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import { API } from "aws-amplify";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes");
+  }
 
   function renderNotesList(notes) {
     return null;
